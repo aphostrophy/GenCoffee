2fa90028def738572667bbcf714d111fbe93d0b4
Jesson Yo <Aphostrophy@gmail.com>
2021-11-01 21:31:40 +0700

    phone sign in setup

--- src/screens/Auth/SignInScreen/index.js
+++ src/screens/Auth/SignInScreen/index.js
@@ -1,11 +1,14 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {SafeAreaView} from 'react-native-safe-area-context';
-import {Text, Button} from 'react-native';
+import {Text, Button, TextInput} from 'react-native';
 
 import { GoogleSignin } from '@react-native-google-signin/google-signin';
 import auth from '@react-native-firebase/auth';
 
 const SignInScreen = ({navigation}) => {
+  const [confirm, setConfirm] = useState(null);
+
+  const [code, setCode] = useState('');
   async function onGoogleButtonPress() {
     // Get the users ID token
     const { idToken } = await GoogleSignin.signIn();
@@ -17,9 +20,28 @@ const SignInScreen = ({navigation}) => {
     return auth().signInWithCredential(googleCredential);
   }
 
+  async function signInWithPhoneNumber(phoneNumber) {
+    const confirmation = await auth().signInWithPhoneNumber(phoneNumber);
+    setConfirm(confirmation);
+  }
+
+  async function confirmCode() {
+    try {
+      await confirm.confirm(code);
+    } catch (error) {
+      console.log('Invalid code.');
+    }
+  }
+
   return (
     <SafeAreaView>
       <Text>SignIn Screen</Text>
+      <Button
+        title="Phone Number Sign In"
+        onPress={() => signInWithPhoneNumber('')} //Format : +62XXXXXXXXXX
+      />
+      <TextInput value={code} onChangeText={text => setCode(text)} />
+      <Button title="Confirm Code" onPress={() => confirmCode()} />
       <Button
         title="Google Sign-In"
         onPress={() => onGoogleButtonPress().then(() => console.log('Signed in with Google!'))}
