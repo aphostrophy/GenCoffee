e2cbaeb160463f4acffdd539a90bbd5b3dbfd030
Jesson Yo <Aphostrophy@gmail.com>
2021-12-01 15:27:05 +0700

    refactor

--- src/screens/Dashboard/MenuScreen/deliveryCard.tsx
+++ src/screens/Dashboard/MenuScreen/deliveryCard.tsx
@@ -1,17 +1,10 @@
 import React from 'react';
-import { View, StyleSheet, Text, Image } from 'react-native';
+import { View, Text, Image } from 'react-native';
+import { TouchableOpacity } from 'react-native-gesture-handler';
 import LinearGradient from 'react-native-linear-gradient';
 
 import { Spacer } from '@components';
-import {
-  PoppinsBold,
-  Poppins,
-  Montserrat,
-  MontserratBold,
-  MontserratSemiBold,
-} from '@styles/fonts';
-import { WHITE, YELLOW } from '@styles/colors';
-import { TouchableOpacity } from 'react-native-gesture-handler';
+import { deliveryCardStyles as styles } from './styles';
 
 const DeliveryCard = (): JSX.Element => {
   return (
@@ -49,95 +42,5 @@ const MiniCard = (): JSX.Element => {
     </View>
   );
 };
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    borderRadius: 8,
-    paddingHorizontal: 25,
-    paddingVertical: 18,
-    justifyContent: 'center',
-  },
-  row: {
-    flexDirection: 'row',
-    alignItems: 'center',
-  },
-  column: {
-    flexDirection: 'column',
-  },
-  topSection: {
-    flex: 1,
-  },
-  bottomSection: {
-    flex: 2,
-  },
-  title: {
-    fontFamily: MontserratBold,
-    color: WHITE,
-    fontSize: 20,
-  },
-  subtitle: {
-    fontFamily: MontserratSemiBold,
-    color: WHITE,
-    fontSize: 14,
-  },
-  icon: {
-    width: 25,
-    height: 25,
-  },
-  miniCardContainer: {
-    flex: 1,
-    width: '70%',
-    justifyContent: 'center',
-  },
-  miniCardTop: {
-    backgroundColor: YELLOW,
-    flex: 1,
-    paddingHorizontal: 10,
-    alignItems: 'center',
-    justifyContent: 'center',
-    borderTopLeftRadius: 8,
-    borderTopRightRadius: 8,
-  },
-  miniCardBottom: {
-    backgroundColor: WHITE,
-    flex: 2,
-    paddingHorizontal: 10,
-    alignItems: 'center',
-    justifyContent: 'space-between',
-    borderBottomLeftRadius: 8,
-    borderBottomRightRadius: 8,
-  },
-
-  miniCardTopTitle: {
-    color: WHITE,
-    fontFamily: PoppinsBold,
-    fontSize: 12,
-  },
-
-  miniCardBottomLabel: {
-    fontFamily: PoppinsBold,
-    includeFontPadding: false,
-    padding: 0,
-    fontSize: 14,
-  },
-  miniCardBottomSmallLabel: {
-    fontFamily: Poppins,
-    includeFontPadding: false,
-    padding: 0,
-    fontSize: 12,
-  },
-  miniCardBottomButton: {
-    backgroundColor: YELLOW,
-    borderRadius: 5,
-    paddingHorizontal: 8,
-    paddingVertical: 2,
-  },
-  miniCardBottomButtonText: {
-    fontFamily: PoppinsBold,
-    color: WHITE,
-    fontSize: 12,
-  },
-});
 
 export { DeliveryCard };
---
+++ src/screens/Dashboard/MenuScreen/styles.tsx
@@ -0,0 +1,95 @@
+import { StyleSheet } from 'react-native';
+import { PoppinsBold, Poppins, MontserratBold, MontserratSemiBold } from '@styles/fonts';
+import { WHITE, YELLOW } from '@styles/colors';
+
+const deliveryCardStyles = StyleSheet.create({
+  container: {
+    flex: 1,
+    borderRadius: 8,
+    paddingHorizontal: 25,
+    paddingVertical: 18,
+    justifyContent: 'center',
+  },
+  row: {
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+  column: {
+    flexDirection: 'column',
+  },
+  topSection: {
+    flex: 1,
+  },
+  bottomSection: {
+    flex: 2,
+  },
+  title: {
+    fontFamily: MontserratBold,
+    color: WHITE,
+    fontSize: 20,
+  },
+  subtitle: {
+    fontFamily: MontserratSemiBold,
+    color: WHITE,
+    fontSize: 14,
+  },
+  icon: {
+    width: 25,
+    height: 25,
+  },
+  miniCardContainer: {
+    flex: 1,
+    width: '70%',
+    justifyContent: 'center',
+  },
+  miniCardTop: {
+    backgroundColor: YELLOW,
+    flex: 1,
+    paddingHorizontal: 10,
+    alignItems: 'center',
+    justifyContent: 'center',
+    borderTopLeftRadius: 8,
+    borderTopRightRadius: 8,
+  },
+  miniCardBottom: {
+    backgroundColor: WHITE,
+    flex: 2,
+    paddingHorizontal: 10,
+    alignItems: 'center',
+    justifyContent: 'space-between',
+    borderBottomLeftRadius: 8,
+    borderBottomRightRadius: 8,
+  },
+
+  miniCardTopTitle: {
+    color: WHITE,
+    fontFamily: PoppinsBold,
+    fontSize: 12,
+  },
+
+  miniCardBottomLabel: {
+    fontFamily: PoppinsBold,
+    includeFontPadding: false,
+    padding: 0,
+    fontSize: 14,
+  },
+  miniCardBottomSmallLabel: {
+    fontFamily: Poppins,
+    includeFontPadding: false,
+    padding: 0,
+    fontSize: 12,
+  },
+  miniCardBottomButton: {
+    backgroundColor: YELLOW,
+    borderRadius: 5,
+    paddingHorizontal: 8,
+    paddingVertical: 2,
+  },
+  miniCardBottomButtonText: {
+    fontFamily: PoppinsBold,
+    color: WHITE,
+    fontSize: 12,
+  },
+});
+
+export { deliveryCardStyles };
