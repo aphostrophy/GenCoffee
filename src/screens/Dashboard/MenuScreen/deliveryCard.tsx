cf1a65422ed8089e24dc507cc8671a2ed27abdea
Jesson Yo <Aphostrophy@gmail.com>
2021-12-01 15:22:33 +0700

    delivery card

--- src/screens/Dashboard/MenuScreen/deliveryCard.tsx
+++ src/screens/Dashboard/MenuScreen/deliveryCard.tsx
@@ -3,8 +3,14 @@ import { View, StyleSheet, Text, Image } from 'react-native';
 import LinearGradient from 'react-native-linear-gradient';
 
 import { Spacer } from '@components';
-import { PoppinsBold, Poppins } from '@styles/fonts';
-import { WHITE } from '@styles/colors';
+import {
+  PoppinsBold,
+  Poppins,
+  Montserrat,
+  MontserratBold,
+  MontserratSemiBold,
+} from '@styles/fonts';
+import { WHITE, YELLOW } from '@styles/colors';
 import { TouchableOpacity } from 'react-native-gesture-handler';
 
 const DeliveryCard = (): JSX.Element => {
@@ -15,8 +21,9 @@ const DeliveryCard = (): JSX.Element => {
         <Spacer width={10} />
         <Text style={styles.title}>Diantar</Text>
         <Spacer width={10} />
-        <Text style={styles.subtitle}>{`ganti ke 'Ambil Sendiri' >`}</Text>
+        <Text style={styles.subtitle}>{`ganti ke 'Ambil Sendiri'`}</Text>
       </View>
+      <Spacer height={15} />
       <View style={styles.bottomSection}>
         <MiniCard />
       </View>
@@ -27,16 +34,16 @@ const DeliveryCard = (): JSX.Element => {
 const MiniCard = (): JSX.Element => {
   return (
     <View style={styles.miniCardContainer}>
-      <View style={styles.row}>
-        <Text>Diantar ke</Text>
+      <View style={[styles.row, styles.miniCardTop]}>
+        <Text style={styles.miniCardTopTitle}>Diantar ke</Text>
       </View>
-      <View style={styles.row}>
+      <View style={[styles.row, styles.miniCardBottom]}>
         <View style={styles.column}>
-          <Text>Jl. Ganesha no 10</Text>
-          <Text>Dago, Bandung</Text>
+          <Text style={styles.miniCardBottomLabel}>Jl. Ganesha no 10</Text>
+          <Text style={styles.miniCardBottomSmallLabel}>Dago, Bandung</Text>
         </View>
-        <TouchableOpacity>
-          <Text>Ganti</Text>
+        <TouchableOpacity style={styles.miniCardBottomButton}>
+          <Text style={styles.miniCardBottomButtonText}>Ganti</Text>
         </TouchableOpacity>
       </View>
     </View>
@@ -65,12 +72,12 @@ const styles = StyleSheet.create({
     flex: 2,
   },
   title: {
-    fontFamily: PoppinsBold,
+    fontFamily: MontserratBold,
     color: WHITE,
     fontSize: 20,
   },
   subtitle: {
-    fontFamily: Poppins,
+    fontFamily: MontserratSemiBold,
     color: WHITE,
     fontSize: 14,
   },
@@ -80,8 +87,56 @@ const styles = StyleSheet.create({
   },
   miniCardContainer: {
     flex: 1,
-    width: '60%',
+    width: '70%',
     justifyContent: 'center',
+  },
+  miniCardTop: {
+    backgroundColor: YELLOW,
+    flex: 1,
+    paddingHorizontal: 10,
+    alignItems: 'center',
+    justifyContent: 'center',
+    borderTopLeftRadius: 8,
+    borderTopRightRadius: 8,
+  },
+  miniCardBottom: {
+    backgroundColor: WHITE,
+    flex: 2,
+    paddingHorizontal: 10,
+    alignItems: 'center',
+    justifyContent: 'space-between',
+    borderBottomLeftRadius: 8,
+    borderBottomRightRadius: 8,
+  },
+
+  miniCardTopTitle: {
+    color: WHITE,
+    fontFamily: PoppinsBold,
+    fontSize: 12,
+  },
+
+  miniCardBottomLabel: {
+    fontFamily: PoppinsBold,
+    includeFontPadding: false,
+    padding: 0,
+    fontSize: 14,
+  },
+  miniCardBottomSmallLabel: {
+    fontFamily: Poppins,
+    includeFontPadding: false,
+    padding: 0,
+    fontSize: 12,
+  },
+  miniCardBottomButton: {
+    backgroundColor: YELLOW,
+    borderRadius: 5,
+    paddingHorizontal: 8,
+    paddingVertical: 2,
+  },
+  miniCardBottomButtonText: {
+    fontFamily: PoppinsBold,
+    color: WHITE,
+    fontSize: 12,
   },
 });
 
