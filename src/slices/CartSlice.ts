cdb0d3c77150a842f4ba49c398fe043225e72fb6
Jesson Yo <Aphostrophy@gmail.com>
2021-12-08 13:58:14 +0700

    add product to slice

--- src/slices/CartSlice.ts
+++ src/slices/CartSlice.ts
@@ -1,17 +1,30 @@
 import { createSlice, PayloadAction } from '@reduxjs/toolkit';
 
-type ReducedProductData = {
+interface DenormalizedProductData {
+  id: string;
+  name: string;
+  quantity: number;
+  options: Record<string, string>;
+}
+
+type NormalizedProductData = Omit<DenormalizedProductData, 'id'>;
+
+interface ProductTypeData {
+  imagePath: string;
+  availableOptions: Record<string, Array<string>>;
+  variants: Array<NormalizedProductData>;
+}
+
+interface AddProductPayload {
   id: string;
   name: string;
   imagePath: string;
   quantity: number;
-};
+  options: Record<string, Array<{ option: string; selected: boolean }>>;
+}
 
-/**
- * items is an object with product id key and ReducedProductData value.
- */
 interface CartState {
-  items: Record<string, ReducedProductData>;
+  items: Record<string, ProductTypeData>;
   itemCount: number;
 }
 
@@ -28,17 +41,59 @@ const cartSlice = createSlice({
       state.items = {};
       state.itemCount = 0;
     },
-    addProductToCart: (state, action: PayloadAction<ReducedProductData>) => {
-      const { id, name, imagePath, quantity } = action.payload;
+    addProductToCart: (state, action: PayloadAction<AddProductPayload>) => {
+      const { id, name, imagePath, quantity, options } = action.payload;
+
       state.itemCount += quantity;
+
+      const optionsKeys = Object.keys(options);
+      const availableOptions: Record<string, Array<string>> = {};
+      const selectedOptions: Record<string, string> = {};
+
+      for (let i = 0; i < optionsKeys.length; i++) {
+        const key = optionsKeys[i];
+        const arr = [];
+        let selected = '';
+        for (let j = 0; j < options[key].length; j++) {
+          arr.push(options[key][j].option);
+          if (options[key][j].selected) {
+            selected = options[key][j].option;
+          }
+        }
+        availableOptions[key] = arr;
+        selectedOptions[key] = selected;
+      }
+
+      console.log(availableOptions);
+      console.log(selectedOptions);
+
       if (Object.prototype.hasOwnProperty.call(state.items, id)) {
-        state.items[id].quantity += quantity;
+        let found = false;
+        const productTypeData = state.items[id];
+
+        for (let i = 0; i < productTypeData.variants.length; i++) {
+          let isEqual = true;
+          for (let j = 0; j < optionsKeys.length; j++) {
+            const key = optionsKeys[j];
+            if (productTypeData.variants[i].options[key] !== selectedOptions[key]) {
+              isEqual = false;
+            }
+          }
+          if (isEqual) {
+            productTypeData.variants[i].quantity += quantity;
+            found = true;
+            break;
+          }
+        }
+
+        if (!found) {
+          state.items[id].variants.push({ name, quantity, options: selectedOptions });
+        }
       } else {
         state.items[id] = {
-          id,
-          name,
           imagePath,
-          quantity,
+          availableOptions,
+          variants: [{ name, quantity, options: selectedOptions }],
         };
       }
     },
@@ -46,11 +101,10 @@ const cartSlice = createSlice({
       const id = action.payload;
       if (Object.prototype.hasOwnProperty.call(state.items, id)) {
         state.itemCount--;
-        if (state.items[id].quantity > 1) {
-          state.items[id].quantity--;
-        } else {
-          delete state.items[id];
-        }
+
+        const productTypeData = state.items[id];
+
+        for (let i = 0; productTypeData.variants.length; i++) {}
       }
     },
     removeProductFromCart: (state, action: PayloadAction<string>) => {
