be88b06355bb54168c2a294d5ea05b837b6731a2
Jesson Yo <Aphostrophy@gmail.com>
2021-12-17 09:57:10 +0700

    setup push notification

--- index.js
+++ index.js
@@ -5,5 +5,30 @@
 import { AppRegistry } from 'react-native';
 import App from './App';
 import { name as appName } from './app.json';
+import PushNotification from 'react-native-push-notification';
+
+PushNotification.configure({
+  onNotification: function (notification) {
+    console.log('NOTIFICATION:', notification);
+  },
+  popInitialNotification: true,
+
+  /**
+   * (optional) default: true
+   * - Specified if permissions (ios) and token (android and ios) will requested or not,
+   * - if not, you must call PushNotificationsHandler.requestPermissions() later
+   * - if you are not using remote notification or do not have Firebase installed, use this:
+   *     requestPermissions: Platform.OS === 'ios'
+   */
+  requestPermissions: true,
+});
+
+PushNotification.createChannel(
+  {
+    channelId: 'gen-coffee', // (required)
+    channelName: 'gen-coffee', // (required)
+  },
+  created => console.log(`CreateChannel returned '${created}'`),
+);
 
 AppRegistry.registerComponent(appName, () => App);
