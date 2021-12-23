fba5a4096ca7f9136e4623910b8892ac993f3a9e
Jesson Yo <Aphostrophy@gmail.com>
2021-12-23 20:45:32 +0700

    reworked firebase data source hook

--- src/hooks/Data/useFirebaseDataSource.ts
+++ src/hooks/Data/useFirebaseDataSource.ts
@@ -1,18 +1,56 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect, useRef } from 'react';
 import { FirebaseFirestoreTypes } from '@react-native-firebase/firestore';
+import { Observable, Subscription } from 'rxjs';
 
-export function useFirebaseDataSource<T>(
+interface FirebaseDocumentInterface {
+  id: string;
+}
+
+export function useFirebaseDataSource<T extends FirebaseDocumentInterface>(
   getResourceFunc: () => Promise<FirebaseFirestoreTypes.QuerySnapshot>,
 ): T[] | null {
+  const [subscription, setSubscription] = useState<null | Subscription>(null);
   const [resource, setResource] = useState<null | T[]>(null);
 
   useEffect(() => {
-    (async () => {
-      const snapshot = await getResourceFunc();
-      const result: T[] = snapshot.docs.map(data => data.data() as T);
-      setResource(result);
-    })();
+    const observable = new Observable<
+      FirebaseFirestoreTypes.QuerySnapshot<FirebaseFirestoreTypes.DocumentData>
+    >(observer => {
+      getResourceFunc()
+        .then(snapshot => {
+          observer.next(snapshot);
+          observer.complete();
+        })
+        .catch((err: unknown) => {
+          if (err instanceof Error) {
+            observer.error(err);
+          }
+        });
+    });
+    setSubscription(
+      observable.subscribe({
+        next: (
+          snapshot: FirebaseFirestoreTypes.QuerySnapshot<FirebaseFirestoreTypes.DocumentData>,
+        ) => {
+          const result: T[] = snapshot.docs.map(data => ({ ...data.data(), id: data.id } as T));
+          setResource(result);
+        },
+        error: err => {
+          if (err instanceof Error) {
+            console.log(`Error at use firebase data source observable ${err.message}`);
+          }
+        },
+      }),
+    );
   }, [getResourceFunc]);
+
+  useEffect(() => {
+    return () => {
+      if (subscription) {
+        subscription.unsubscribe();
+      }
+    };
+  }, [subscription]);
 
   return resource;
 }
